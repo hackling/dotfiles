a8c11c3513454935e1137af232864e2c6f41ee5f
Nicholas Chmielewski <hackling@gmail.com>
2015-01-13 12:58:31 +1000

    Run bundle exec on rubocop to ensure correct version is used

--- config/aliases.sh
+++ config/aliases.sh
@@ -2,5 +2,5 @@ alias v="vim"
 alias whatalias=" alias | grep"
 alias xx="exit"
 alias killrspec="pkill -fl rspec"
-alias fastcop='git diff --name-only origin/develop | grep \\.rb$ | xargs ls 2>/dev/null | xargs rubocop --config .rubocop.yml --force-exclusion'
+alias fastcop='git diff --name-only origin/develop | grep \\.rb$ | xargs ls 2>/dev/null | xargs bundle exec rubocop --config .rubocop.yml --force-exclusion'
 alias checkhaml='bundle exec haml-lint $(find app/views/* -maxdepth 0 -type d -not -name kss)'
