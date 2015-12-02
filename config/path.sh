d9af1d4aa8de68cb06bd2541b6a6b0220646497c
Nicholas Chmielewski <hackling@gmail.com>
2015-12-03 09:04:14 +1000

    Add some useful comments

--- config/path.sh
+++ config/path.sh
@@ -1,5 +1,8 @@
+# Add EDH specific applications to PATH
 PATH="/usr/local/opt/coreutils/libexec/gnubin:$PATH"
 PATH="~/code/plain-utils/bin:$PATH"
+
+# Source Chruby
 source /usr/local/share/chruby/chruby.sh
 source /usr/local/share/chruby/auto.sh
 
