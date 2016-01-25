10aea07a4dabbf1938e652bd94829426b0454ba4
Nicholas Chmielewski <hackling@gmail.com>
2016-01-25 13:11:09 +1000

    Remove fastcop alias from dotfiles

--- config/aliases.sh
+++ config/aliases.sh
@@ -3,5 +3,4 @@ alias vim="nvim -u ~/.vimrc"
 alias whatalias=" alias | grep"
 alias xx="exit"
 alias killrspec="pkill -fl rspec"
-alias fastcop='git diff --name-only origin/develop | grep \\.rb$ | xargs ls 2>/dev/null | xargs bundle exec rubocop --config .rubocop.yml --force-exclusion'
 alias checkhaml='bundle exec haml-lint $(find app/views/* -maxdepth 0 -type d -not -name kss)'
