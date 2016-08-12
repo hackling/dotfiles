40d1c64d3fd2acd3e3853c3ee088b65fea2b26c9
Nick Chmielewski <hackling@gmail.com>
2016-08-12 14:02:30 +1000

    Update cleanimages alias

--- config/aliases.sh
+++ config/aliases.sh
@@ -5,4 +5,4 @@ alias xx="exit"
 alias killrspec="pkill -fl rspec"
 alias checkhaml='bundle exec haml-lint $(find app/views/* -maxdepth 0 -type d -not -name kss)'
 alias cleancontainers="docker ps -a | grep Exited | awk '{print $1}' | xargs docker rm"
-alias cleanimages="docker images | awk '$1 == \"<none>\" {print $3}' | xargs docker rmi"
+alias cleanimages="docker images | grep '<none>' | awk {'print $3'} | xargs -n 1 docker rmi"
