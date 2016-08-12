fddde94fca686b5389ae0094403f76e5a2cdf95f
Nicholas Chmielewski <hackling@gmail.com>
2016-08-12 10:18:39 +1000

    Add aliases to clean up docker

--- config/aliases.sh
+++ config/aliases.sh
@@ -4,3 +4,5 @@ alias whatalias=" alias | grep"
 alias xx="exit"
 alias killrspec="pkill -fl rspec"
 alias checkhaml='bundle exec haml-lint $(find app/views/* -maxdepth 0 -type d -not -name kss)'
+alias cleancontainers="docker ps -a | grep Exited | awk '{print $1}' | xargs docker rm"
+alias cleanimages="docker images | awk '$1 == \"<none>\" {print $3}' | xargs docker rmi"
