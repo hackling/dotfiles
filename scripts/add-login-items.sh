d0d2efa186b63ad8b425a130635cc89c0a17456c
Nick Chmielewski <hackling@gmail.com>
2017-11-29 11:58:09 +1000

    Update login items

--- scripts/add-login-items.sh
+++ scripts/add-login-items.sh
@@ -1,4 +1,4 @@
 echo 'Adding Login Items'
-osascript -e 'tell application "System Events" to make new login item at end with properties {path:"/Applications/Alfred 2.app", name:"Alfred", hidden:true}'
+osascript -e 'tell application "System Events" to make new login item at end with properties {path:"/Applications/Alfred 3.app", name:"Alfred", hidden:true}'
 osascript -e 'tell application "System Events" to make new login item at end with properties {path:"/Applications/Divvy.app", name:"Divvy", hidden:true}'
-osascript -e 'tell application "System Events" to make new login item at end with properties {path:"/Applications/Karabiner.app", name:"Karabiner", hidden:true}'
+osascript -e 'tell application "System Events" to make new login item at end with properties {path:"/Applications/Hammerspoon.app", name:"Alfred", hidden:true}'
