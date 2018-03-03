56679905cd3a834772387c866989c4949638d02d
Nick Chmielewski <hackling@gmail.com>
2018-03-03 14:21:28 +1000

    WIP

--- config/aliases.sh
+++ config/aliases.sh
@@ -1,9 +1,6 @@
-alias v="nvim -u ~/.vimrc"
-alias vim="nvim -u ~/.vimrc"
 alias whatalias=" alias | grep"
 alias xx="exit"
 alias killrspec="pkill -fl rspec"
-alias checkhaml='bundle exec haml-lint $(find app/views/* -maxdepth 0 -type d -not -name kss)'
 alias cleancontainers="docker ps -a | grep Exited | awk '{print $1}' | xargs docker rm"
 alias cleanimages="docker images | grep '<none>' | awk {'print $3'} | xargs -n 1 docker rmi"
 alias olga="say --voice=Milena"
