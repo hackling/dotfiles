2b6dba351ff449fbfb4b4110d7e8e952483f908a
Nicholas Chmielewski <hackling@gmail.com>
2019-11-28 11:07:12 +1000

    Remove unused files

--- bash-history-ordered-by-usage.sh
+++
@@ -1,1 +0,0 @@
-cat .bash_history.back  | sort | uniq -c | sort | awk '{first = $1; $1 = ""; print $0 }' > .bash_history_ordered
--- ssh-fix.sh
+++
@@ -1,4 +0,0 @@
-if [ -S "$SSH_AUTH_SOCK" ] && [ ! -h "$SSH_AUTH_SOCK" ]; then
-    ln -sf "$SSH_AUTH_SOCK" ~/.ssh/ssh_auth_sock
-fi
-export SSH_AUTH_SOCK=~/.ssh/ssh_auth_sock
